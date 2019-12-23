5e94f3ae9a2f18f79c928ffdd231ff0f263623e2
Ivan Molodetskikh <yalterz@gmail.com>
2019-12-24 00:00:09 +0300

    rs: make read and write mods private

--- hltas/src/lib.rs
+++ hltas/src/lib.rs
@@ -1,10 +1,10 @@
 //! A crate for reading and writing Half-Life TAS scripts (`.hltas`).
 
-pub mod read;
-pub mod write;
-
 pub mod types;
 pub use types::HLTAS;
+
+mod read;
+mod write;
 
 #[cfg(not(test))]
 pub mod capi;
