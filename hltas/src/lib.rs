5a0fe990b14adee9c406d991cd2528660158a2bb
Ivan Molodetskikh <yalterz@gmail.com>
2019-12-24 00:12:32 +0300

    rs: add an example to the top-level docs

--- hltas/src/lib.rs
+++ hltas/src/lib.rs
@@ -1,4 +1,37 @@
 //! A crate for reading and writing Half-Life TAS scripts (`.hltas`).
+//!
+//! # Examples
+//!
+//! ```
+//! # extern crate hltas_rs;
+//! # fn foo() -> Result<(), Box<dyn std::error::Error>> {
+//! use hltas_rs::{HLTAS, types::{Line, JumpBug}};
+//!
+//! let contents = "\
+//! version 1
+//! demo test
+//! frames
+//! ------b---|------|------|0.001|-|-|5";
+//!
+//! match HLTAS::from_str(&contents) {
+//!     Ok(hltas) => {
+//!         assert_eq!(hltas.properties.demo, Some("test"));
+//!
+//!         if let Line::FrameBulk(frame_bulk) = hltas.lines[0] {
+//!             assert_eq!(frame_bulk.auto_actions.jump_bug, Some(JumpBug { times: 0 }));
+//!             assert_eq!(frame_bulk.frame_time, "0.001");
+//!             assert_eq!(frame_bulk.frame_count.get(), 5);
+//!         } else {
+//!             unreachable!()
+//!         }
+//!     }
+//!
+//!     // The errors are pretty-printed with context.
+//!     Err(error) => println!("{}", error),
+//! }
+//! # Ok(())
+//! # }
+//! ```
 
 pub mod types;
 pub use types::HLTAS;
