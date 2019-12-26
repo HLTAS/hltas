3b18bc99eb9897734906b277d2660fb00e43a0f2
Ivan Molodetskikh <yalterz@gmail.com>
2019-12-26 14:35:16 +0300

    Update fuzzing files

--- fuzz/fuzz_targets/parse.rs
+++ fuzz/fuzz_targets/parse.rs
@@ -1,10 +1,10 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use hltas::read::hltas;
+use hltas::HLTAS;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
-        let _ = hltas(s);
+        let _ = HLTAS::from_str(s);
     }
 });
--- fuzz/fuzz_targets/read_write_read.rs
+++ fuzz/fuzz_targets/read_write_read.rs
@@ -1,15 +1,17 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use hltas::{read, write};
+use std::str::from_utf8;
+
+use hltas::HLTAS;
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok(s) = std::str::from_utf8(data) {
-        if let Ok((_, hltas)) = read::hltas(s) {
+    if let Ok(s) = from_utf8(data) {
+        if let Ok(hltas) = HLTAS::from_str(s) {
             let mut output = Vec::new();
-            write::hltas(&mut output, &hltas).unwrap();
-            let output = std::str::from_utf8(&output).unwrap();
-            let (_, hltas_2) = read::hltas(&output).unwrap();
+            hltas.to_writer(&mut output).unwrap();
+
+            let hltas_2 = HLTAS::from_str(from_utf8(&output).unwrap()).unwrap();
             assert_eq!(hltas, hltas_2);
         }
     }
