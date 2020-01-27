4402104d1f436f80d81ecbd9c71adf1f2200349d
Ivan Molodetskikh <yalterz@gmail.com>
2020-01-27 16:39:44 +0300

    cpp-bridge: set target for bindings generator

--- gen-bindings.sh
+++ gen-bindings.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 exec bindgen cpp/src/hltas.hpp \
-	-o hltas-cpp-bridge/src/hltas_cpp.rs \
+	-o hltas-cpp-bridge/src/hltas_cpp_raw.rs \
 	--whitelist-function 'hltas_.*' \
 	--whitelist-type 'HLTAS::ErrorDescription' \
 	--rustified-enum 'HLTAS::StrafeType|StrafeDir|ButtonState|Button|ErrorCode|StrafingAlgorithm|ConstraintsType' \
 	--disable-name-namespacing \
-	-- -std=c++14 -I/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/9.2.0/include
+	-- -std=c++14 -I/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/9.2.0/include --target=i686-unknown-linux-gnu
